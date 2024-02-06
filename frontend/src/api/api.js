c34b0af7a9f5ce9aabab6953f9e01dc868fa6071
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2024-02-05 21:19:01 -0300

    Integração API frontend
    Faz com que frontend consiga chamar APIs do backend com uma função de uso geral, perapara e configura CORS para desenvolvimento no backend e no frontend (Apenas em modo desenvolvimento em ambos os ambientes).

---
+++ frontend/src/api/api.js
@@ -0,0 +1,30 @@
+async function chamarAPI(rota, verbo, argsQuery = null, argsForm = null)
+{
+	rota = import.meta.env.VITE_URL_API + `${rota}${argsQuery ? `?${new URLSearchParams(argsQuery)}` : '/' }`;
+	verbo = verbo.trim().toUpperCase();
+
+	let corpo = null;
+	let cabecalhos = {};
+
+	if (verbo != 'GET' && argsForm) {
+		cabecalhos['Content-Type'] = 'application/x-www-form-urlencoded';
+		corpo = new URLSearchParams(argsForm);
+	}
+
+	let res = await fetch(rota, {
+		method: verbo,
+		headers: cabecalhos,
+		body: corpo,
+		mode: import.meta.env.DEV ? 'cors' : 'same-origin',
+		credentials: import.meta.env.DEV ? 'include' : 'same-origin'
+	})
+
+	let json = await res.json().catch(() => null);
+	return {
+		ok: res.ok,
+		status: res.status,
+		resposta: json
+	};
+}
+
+export { chamarAPI };
